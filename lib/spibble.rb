762a322c83058f738cca6bf8f32fcee5815ea314
Curtis McEnroe <programble@gmail.com>
2013-05-17 01:19:26 -0400

    Load database in Application

--- lib/spibble.rb
+++ lib/spibble.rb
@@ -9,6 +9,8 @@ module Spibble
     def initialize
       FileUtils.mkdir_p(SPIBBLE_DIR)
 
+      @database = Database.new(File.join(SPIBBLE_DIR, 'albums.yml'))
+
       begin
         @lastfm = Lastfm.new(File.join(SPIBBLE_DIR, 'session'))
       rescue Lastfm::ApiError => e
