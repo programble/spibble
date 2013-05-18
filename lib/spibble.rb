5183610238056c83ddaaccd48136423e9c1aba64
Curtis McEnroe <programble@gmail.com>
2013-05-18 17:31:47 -0400

    Add scrobbling and listing albums

--- lib/spibble.rb
+++ lib/spibble.rb
@@ -1,6 +1,6 @@
 require 'fileutils'
 
-require 'spibble/lastfm'
+%w[database lastfm import].each {|r| require "spibble/#{r}" }
 
 module Spibble
   SPIBBLE_DIR = File.join(Dir.home, '.config', 'spibble')
@@ -16,6 +16,41 @@ module Spibble
       rescue Lastfm::ApiError => e
         puts "Error: #{e}"
         exit 1
+      end
+    end
+
+    def list
+      @database.each do |album|
+        puts "#{album.artist} - #{album.title}"
+      end
+    end
+
+    def add
+    end
+
+    def import(files)
+    end
+
+    def scrobble(album)
+      album = @database.find(album)
+      unless album
+        puts 'Error: could not find album in database'
+        exit 1
+      end
+
+      puts album
+      puts
+
+      album.tracks.each do |track|
+        if side = album.sides[track.number]
+          print "Scrobble#{' side' unless side[' ']} #{side}? "
+          STDIN.gets # TODO: Handle input
+        end
+
+        puts track
+        @lastfm.now_playing(album, track)
+        sleep(track.length)
+        @lastfm.scrobble
       end
     end
   end
