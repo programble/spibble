2260ff0d997765d0eec6144bca27477a79418870
Curtis McEnroe <programble@gmail.com>
2013-06-29 16:33:32 -0400

    Add 404 page

--- src/spibble/server.clj
+++ src/spibble/server.clj
@@ -1,5 +1,6 @@
 (ns spibble.server
-  (:require [noir.util.middleware :refer [wrap-strip-trailing-slash]]
+  (:require [me.raynes.laser :as l]
+            [noir.util.middleware :refer [wrap-strip-trailing-slash]]
             [ring.middleware.params :refer [wrap-params]]
             [noir.session :refer [wrap-noir-session wrap-noir-flash] :as session]
             [monger.core :as mg]
@@ -11,13 +12,19 @@
 (let [uri (get (System/getenv) "MONGOLAB_URI" "mongodb://127.0.0.1/spibble_development")]
   (mg/connect-via-uri! uri))
 
-(require '[spibble.views.login :refer [login-routes]]
+(require '[spibble.views.common :refer [layout template]]
+         '[spibble.views.login :refer [login-routes]]
          '[spibble.views.album :refer [album-routes]])
+
+(let [html (-> (template :404) slurp l/unescaped)]
+  (defn four-zero-four [req]
+    (layout html)))
 
 (def handler
   (-> (routes login-routes
               album-routes
-              (resources "/"))
+              (resources "/")
+              (not-found four-zero-four))
       (wrap-params)
       (wrap-noir-flash)
       (wrap-noir-session {:store (monger-store "sessions")})
