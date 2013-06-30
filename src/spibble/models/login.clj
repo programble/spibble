b7eab477969e615e169efc20f0415f2ef016c097
Curtis McEnroe <programble@gmail.com>
2013-06-30 06:33:52 -0400

    Clean up login model

--- src/spibble/models/login.clj
+++ src/spibble/models/login.clj
@@ -1,8 +1,8 @@
 (ns spibble.models.login
-  (:require [spibble.utilities :refer [image-map]]
+  (:require [spibble.utilities [image-map]]
             [spibble.config :refer [api-key api-secret]]
-            [noir.session :as session]
             [monger.collection :as mc]
+            [noir.session :as session]
             [me.raynes.least :as least]
             [me.raynes.least.authorize :as auth]))
 
@@ -10,23 +10,22 @@
 
 (mc/ensure-index "users" {:session 1})
 
-(defn user-map [user info]
-    (merge user
-           (select-keys info [:realname :url])
-           {:image (image-map (:image info))}))
+(defn from-lastfm [user]
+  (-> user
+      (select-keys [:realname :url])
+      (assoc :image (image-map (:image user)))))
 
 (defn create-user [session]
   (let [user {:session (:key session)
-              :name (:name session)
-              :foo "penis"}]
+              :name (:name session)}]
     (mc/insert-and-return "users" user)))
 
 (defn login [token]
   (let [session (auth/get-session api-key api-secret token)
         user (or (mc/find-one-as-map "users" {:session (:key session)})
                  (create-user session))
-        info (:user (least/read "user.getInfo" api-key {:secret api-secret :sk (:session user)}))]
-    (session/put! :user (user-map user info))))
+        info (:user (least/read "user.getInfo" api-key {:secret api-secret, :sk (:session user)}))]
+    (session/put! :user (merge user (from-lastfm info)))))
 
 (defn logout []
   (session/remove! :user))
