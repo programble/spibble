fdf7df48f50bef87ea1450218b457b8304ce576e
Curtis McEnroe <programble@gmail.com>
2013-07-01 01:46:44 -0400

    Combine layout HTML

--- src/spibble/views/common.clj
+++ src/spibble/views/common.clj
@@ -1,34 +1,30 @@
 (ns spibble.views.common
-  (:require [noir.session :as session]
-            [me.raynes.laser :refer [defragment] :as l]
-            [clojure.java.io :refer [resource]]))
+  (:require [clojure.java.io :refer [resource]]
+            [me.raynes.laser :as l :refer [defragment]]
+            [noir.session :as session]))
 
 (defn template [file]
   (resource (str "spibble/views/templates/" (name file) ".html")))
 
-(defragment head (template :head)
-  [title]
-  (l/element= :title) (l/content (or title "Spibble")))
-
-(defragment body (template :body)
-  [contents active query]
-  (if-let [user (session/get :user)]
-    [(l/id= :user) (comp (l/attr :href (str "/user/" (:name user)))
-                         (l/content (:name user)))
-     (l/class= :logged-out) (l/remove)]
-    [(l/class= :logged-in) (l/remove)])
-  (when active
-    [(l/id= active) (l/add-class "active")])
-  (when query
-    [(l/class= :search-query) (l/attr :value query)])
-  (l/id= :contents) (l/content contents))
-
-(let [html (l/parse (template :html))]
+(let [html (l/parse (template :layout))]
   (defn layout [content & [{:keys [title active query]}]]
     (l/document
       html
-      (l/element= :head) (l/content (head title))
-      (l/element= :body) (l/content (body content active query)))))
+      (l/element= :title) (l/content (or title "Spibble"))
+
+      (if-let [user (session/get :user)]
+        [(l/id= :user) (comp (l/attr :href (str "/user/" (:name user)))
+                             (l/content (:name user)))
+         (l/class= :logged-out) (l/remove)]
+        [(l/class= :logged-in) (l/remove)])
+
+      (when active
+        [(l/id= active) (l/add-class "active")])
+
+      (when query
+        [(l/class= :search-query) (l/attr :value query)])
+
+      (l/id= :contents) (l/content content))))
 
 (defragment pager (template :pager)
   [base page pages]
