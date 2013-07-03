1a339251edf50b911b51a719a5f181050e1245c1
Curtis McEnroe <programble@gmail.com>
2013-07-03 02:18:25 -0400

    Make the views more awesomer

--- src/spibble/views/album.clj
+++ src/spibble/views/album.clj
@@ -7,81 +7,87 @@
             [noir.response :refer [redirect]]
             [compojure.core :refer [defroutes GET]]))
 
-(defragment api-error (template :api-error)
+(defragment render-api-error (template :api-error)
   [error]
   (l/class= :api-error) (l/content (:message error)))
 
-(defn album-thumb [node {:keys [name artist id image] :as album}]
+(defn album-thumb-node [node {:keys [name artist id image] :as album}]
   (l/at node
         (if (:error album)
-          [(l/class= :media) (l/content (api-error album))]
+          [(l/class= :media) (l/content (render-api-error album))]
           [(l/element= :a) (l/attr :href (str "/album/" id))
-           (l/element= :img) (l/attr :src (:extralarge image))
            (l/class= :album) (l/content name)
-           (l/element= :h4) (l/content artist)])))
+           (l/element= :h4) (l/content artist)
+           (l/element= :img) (l/attr :src (:extralarge image))])))
 
-(let [none (static :none)]
-  (defragment album-thumbs (template :album-thumb)
+(let [none-html (static :none)]
+  (defragment render-album-thumbs (template :album-thumb)
     [albums]
     (l/element= :li) (if (seq albums)
                        #(for [album albums]
-                          (album-thumb % album))
+                          (album-thumb-node % album))
                        (comp (l/attr :class "span12")
-                             (l/content none)))))
+                             (l/content none-html)))))
 
-(defn tracks-table [tracks]
+(defn render-tracks-table [tracks]
   (for [track tracks]
-    (l/node :tr
-            :content [(l/node :td :content (:name track))
-                      (l/node :td :content (str (:duration track)))]))) ; TODO: Format duration
+    (l/node :tr :content [(l/node :td :content (:name track))
+                          ;; TODO: Format duration
+                          (l/node :td :content (str (:duration track)))])))
 
-(defragment show-album (template :album)
-  [album]
-  (l/element= :img) (l/attr :src (-> album :image :extralarge))
-  (l/element= :h1) (l/content (:name album))
-  (l/element= :h2) (l/content (:artist album))
-  (l/element= :table) (l/content (tracks-table (:tracks album))))
+(defragment render-album (template :album)
+  [{:keys [name artist image tracks]}]
+  (l/element= :h1) (l/content name)
+  (l/element= :h2) (l/content artist)
+  (l/element= :img) (l/attr :src (:extralarge image))
+  (l/element= :table) (l/content (render-tracks-table tracks)))
 
-(let [hero [(static :hero-unit)]
+(let [hero-html (static :hero-unit)
       heading (heading-search "Top Albums" "/albums/search")]
-  (defn albums-page [page]
-    (layout
-      (concat (when-not (session/get :user) hero)
-              heading
-              [(l/node :ul :attrs {:class "thumbnails"}
-                       :content (album-thumbs (album/get-top-albums page 6)))]
-              (pager "/albums?" page (count-pages (album/count-albums) 6)))
-      :active :albums)))
+  (defn top-albums-page [page]
+    (let [albums (album/get-top-albums page 6)
+          pages (count-pages (album/count-albums) 6)]
+      (layout
+        (concat (when-not (session/get :user)
+                  [hero-html])
+                heading
+                [(l/node :ul :attrs {:class "thumbnails"}
+                         :content (render-album-thumbs albums))]
+                (pager "/albums?" page pages))
+        :active :albums))))
 
 (defn search-page [query page]
-  (if (seq query)
-    (let [{:keys [albums pages]} (album/search query page 6)]
-      (layout (concat (heading-search (str "Album search: " query) "/albums/search" query)
-                      [(l/node :ul :attrs {:class "thumbnails"}
-                               :content (album-thumbs albums))]
-                      (pager (str "/albums/search?q=" query "&") page pages))
-              :title (str "Album search: " query)
-              :active :albums))
-    (redirect "/albums")))
+  (let [{:keys [albums pages]} (album/search query page 6)
+        title (str "Album search: " query)]
+    (layout
+      (concat (heading-search title "/albums/search" query)
+              [(l/node :ul :attrs {:class "thumbnails"}
+                       :content (render-album-thumbs albums))]
+              (pager (str "/albums/search?q=" query "&") page pages))
+      :title title
+      :active :albums)))
 
 (defn album-page [id]
   (when-let [album (album/get-album id)]
     (if (:error album)
-      (layout (api-error album))
+      (layout (render-api-error album) :active :albums)
       (layout
-        (show-album album)
+        (render-album album)
         :title (:name album)
         :active :albums))))
 
 (defroutes album-routes
   (GET "/" []
-    (albums-page 1))
+    (top-albums-page 1))
 
   (GET "/albums" [p]
-    (albums-page (safe-parse-long p 1)))
+    (when-let [p (if p (safe-parse-long p) 1)]
+      (top-albums-page p)))
 
   (GET "/albums/search" [q p]
-    (search-page q (safe-parse-long p 1)))
+    (when-let [p (if p (safe-parse-long p) 1)]
+      (when (seq q)
+        (search-page q p))))
 
   (GET "/album/:id" [id]
     (album-page (safe-parse-long id))))
--- src/spibble/views/library.clj
+++ src/spibble/views/library.clj
@@ -2,7 +2,7 @@
   (:require [spibble.models.library :as library]
             [spibble.models.user :as user]
             [spibble.views.common :refer [template layout heading-search pager]]
-            [spibble.views.album :refer [album-thumbs]]
+            [spibble.views.album :refer [render-album-thumbs]]
             [spibble.utilities :refer [safe-parse-long count-pages]]
             [me.raynes.laser :as l :refer [defragment]]
             [noir.session :as session]
@@ -10,19 +10,22 @@
             [compojure.core :refer [defroutes GET]]))
 
 (defn self? [user]
-  (= (:name user )(session/get-in [:user :name])))
+  (= (:name user) (session/get-in [:user :name])))
 
 (defn library-page [name page]
   (when-let [user (user/get-local name)]
-    (let [title (str name "'s Library")]
+    (let [library (library/get-library user page 6)
+          pages (count-pages (library/count-library user) 6)
+          title (str name "'s Library")]
       (layout
         (concat (heading-search title (str "/library/" name "/search"))
                 [(l/node :ul :attrs {:class "thumbnails"}
-                         :content (album-thumbs (library/get-library user page 6)))]
-                (pager (str "/library/" name "?") page (count-pages (library/count-library user) 6)))
+                         :content (render-album-thumbs library))]
+                (pager (str "/library/" name "?") page pages))
         :title title
         :active (when (self? user) :library)))))
 
 (defroutes library-routes
   (GET "/library/:user" [user p]
-    (library-page user (safe-parse-long p 1))))
+    (when-let [p (if p (safe-parse-long p) 1)]
+      (library-page user p))))
