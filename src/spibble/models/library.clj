cf26b684d594a7a72bd1c528add45dac1a999d4d
Curtis McEnroe <programble@gmail.com>
2013-07-06 02:31:59 -0400

    Sort library by recent activity on albums

--- src/spibble/models/library.clj
+++ src/spibble/models/library.clj
@@ -3,10 +3,13 @@
             [spibble.models.album :as album]
             [monger.collection :as mc]
             [monger.query :as mq]
-            [monger.operators :refer [$addToSet $pull $in $inc]]))
+            [monger.operators :refer [$addToSet $pull $in $inc $set]]))
 
 (defn add-album [user album]
-  (mc/update "albums" {:id (:id album)} {$inc {:owners 1}})
+  (mc/update "albums"
+             {:id (:id album)}
+             {$inc {:owners 1}
+              $set {(str "activity." (:name user)) (java.util.Date.)}})
   (mc/find-and-modify "users"
                       {:name (:name user)}
                       {$addToSet {:library (:id album)}}
@@ -25,5 +28,5 @@
 (defn get-library [user page per]
   (map album/get-remote (mq/with-collection "albums"
                           (mq/find {:id {$in (vec (:library user))}})
-                          ;; TODO: Sort by recent activity
+                          (mq/sort {(str "activity." (:name user)) -1})
                           (mq/paginate :page page :per-page per))))
