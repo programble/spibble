58021c26ebfbad7d00a0af782b17f1a88a65cbf9
Curtis McEnroe <programble@gmail.com>
2013-07-07 21:30:21 -0400

    Depend on cljmb

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [lib-noir "0.5.6"]
                  [com.novemberain/monger "1.5.0-beta1"]
                  [me.raynes/laser "2.0.0-SNAPSHOT"]
-                 [clj-config "0.2.0"]]
+                 [clj-config "0.2.0"]
+                 [cljmb "0.1.0"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler spibble.server/handler})
